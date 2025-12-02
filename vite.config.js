233696bef48570581230ebfab8e205f1a913c007
ljb2727 <ljb2727@gmail.com>
2025-12-02 15:19:00 +0900

    fix: base path를 /tarot/로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3tarot/', // GitHub Pages 저장소 이름에 맞게 설정
+  base: '/tarot/', // GitHub Pages 저장소 이름에 맞게 설정
 })
