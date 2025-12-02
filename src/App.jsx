32cdf83fc4a83e8a120fd51f3f45b0f041deeb93
ljb2727 <ljb2727@gmail.com>
2025-12-02 15:40:22 +0900

    fix: 라우터 basename 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Result from './pages/Result';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/reading" element={<Reading />} />
