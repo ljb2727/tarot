4fdf46736c47c098c8a464453401a5797ca9fe1d
ljb2727 <ljb2727@gmail.com>
2025-12-05 12:54:12 +0900

    good

---
+++ android/app/src/main/java/com/onepick/tarot/MainActivity.java
@@ -0,0 +1,15 @@
+package com.onepick.tarot;
+
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 웹뷰가 시스템 바(상태바/네비바) 영역을 침범하지 않도록 설정
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+    }
+}
